981b0ff18e86a7c4e777da1e6487a4abf5f06f28
Octavius97 <octaviomorales97@gmail.com>
2017-11-26 01:31:53 -0600

    Resolucion de Errores

--- app/src/main/java/technoyoung/technoyoungone/MainActivity.java
+++ app/src/main/java/technoyoung/technoyoungone/MainActivity.java
@@ -5,9 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.net.Uri;
-import android.widget.MediaController;
-import android.widget.VideoView;
 
 
 public class MainActivity extends AppCompatActivity {
