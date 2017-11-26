7704433def1881c51da304c690b415d4cd50242e
Octavius97 <31940044+Octavius97@users.noreply.github.com>
2017-11-25 20:36:34 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TechnoYoung_One
---
+++ app/src/main/java/technoyoung/technoyoungone/MainActivity.java
@@ -0,0 +1,27 @@
+package technoyoung.technoyoungone;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button boton_salida = (Button)findViewById(R.id.salida_btn);
+        boton_salida.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+    }
+}
